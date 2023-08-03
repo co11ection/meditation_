51b22b92707caf8a510c55197e2e8821997fec6c
Ivan Koshkin <ivan.koshkin35@gmail.com>
2023-08-03 10:43:18 +0300

    added urls and models to app users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=500, blank=True, null=True)
+    login = models.CharField(max_length=100, blank=True, null=True, verbose_name='Логин', unique=True)
+    password = models.CharField(max_length=500, blank=True, null=True)
+    token = models.CharField(max_length=500, blank=True, null=True, unique=True, verbose_name='Токен авторизации')
+    photo = models.ImageField(null=True, blank=True, upload_to="media/", verbose_name='Аватарка')
+    is_active = models.BooleanField(blank=True, default=True, verbose_name='Статус блокировки')
+    phone_number = models.CharField(max_length=100, blank=True, null=True, verbose_name='Телефон', unique=True)
+    email = models.CharField(max_length=100, blank=True, null=True, verbose_name='Почта')
+    code = models.IntegerField(blank=True, null=True)
+    fcm_token = models.CharField(max_length=500, null=True, blank=True)
+
+    class Meta:
+        managed = True
+        db_table = 'users'
+        verbose_name_plural = 'Пользователи'
+        verbose_name = 'Пользователь'
+
+    def __str__(self):
+        return f"{self.username}"
+
+
+class CodePhone(models.Model):
+    login = models.CharField(max_length=100, blank=True, null=True)
+    code = models.IntegerField(blank=True, null=True)
+    is_confirmed = models.BooleanField(max_length=100, blank=True, null=True, default=False)
+    time = models.DateTimeField(auto_now=True)
---
+++ users/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('registration_get_code', views.registration_get_code),
+    path('registration', views.registration),
+    path('auth', views.auth),
+    path('delete_user', views.delete_user),
+    path('get_user', views.get_user),
+    path('state', views.get_state),
+    path('user_fcm', views.fcm_user),
+    path('update_user', views.update_user),
+    path('change_photo', views.change_photo),
+    path('reset_password', views.reset_password),
+    path('check_code', views.check_code),
+    path('reset_password_confirm', views.reset_password_confirm),
+    path('put_phone', views.put_phone),
+    path('info', views.get_info),
+    path('offer', views.get_offer),
+    path('politic', views.get_pol),
+    path('get_confidential', views.get_conf),
+]
