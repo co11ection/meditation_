3d337094a09c603e1309a51c9598cec095c9f7b9
makhotin07 <108171268+makhotin07@users.noreply.github.com>
2023-08-16 16:26:18 +0300

    add

--- wallet/views.py
+++ wallet/views.py
@@ -129,26 +129,7 @@ class WalletTokensView(APIView):
 #
 # def booster_long():
 #     pass
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def send_tokens_to_user(request):
-    sender_id = request.data.get('sender_id')
-    recipient_id = request.data.get('recipient_id')
-    amount_to_send = request.data.get('amount')
 
-    try:
-        sender = CustomUser.objects.get(id=sender_id)
-        recipient = CustomUser.objects.get(id=recipient_id)
-    except CustomUser.DoesNotExist:
-        return Response({'error': 'User not found'},
-                        status=status.HTTP_404_NOT_FOUND)
-    if sender.send_tokens_to_user(recipient, amount_to_send):
-        return Response({'message': 'Токены успешны отправлены'},
-                        status=status.HTTP_200_OK)
-    else:
-        return Response(
-            {'error': 'Недостаточно токенов на балансе отправителя'},
-            status=status.HTTP_400_BAD_REQUEST)
 
 
 def buy_tokens_from_market():
