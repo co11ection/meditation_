ecdd49e2b991033c8debfcdf5820aaae0e7912b0
Ivan Koshkin <ivan.koshkin35@gmail.com>
2023-08-16 16:36:48 +0300

    fix some bugs

--- wallet/views.py
+++ wallet/views.py
@@ -1,10 +1,8 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
 
 from users.models import CustomUser
 from .serializers import WalletSerializer
@@ -129,7 +127,6 @@ class WalletTokensView(APIView):
 #
 # def booster_long():
 #     pass
-
 
 
 def buy_tokens_from_market():
