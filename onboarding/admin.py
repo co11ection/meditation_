7e30475cc6ac408ff8beb75fa67d5715f6b67ee2
makhotin07 <108171268+makhotin07@users.noreply.github.com>
2023-08-17 15:01:36 +0300

    add

--- onboarding/admin.py
+++ onboarding/admin.py
@@ -1,41 +1,9 @@
 from django.contrib import admin
-
-from onboarding.models import ChatMessage, OnboardingText, Complaint
-
-from .models import OnboardingStep, UserOnboarding
+from .models import OnboardingText
 
 
-@admin.register(OnboardingStep)
-class OnboardingStepAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description')
-    search_fields = ('title', 'description')
-
-
-@admin.register(UserOnboarding)
-class UserOnboardingAdmin(admin.ModelAdmin):
-    list_display = ('user', 'get_completed_steps', 'skipped')
-    list_filter = ('skipped',)
-    search_fields = ('user__username', 'user__email')
-
-    def get_completed_steps(self, obj):
-        return ', '.join([step.title for step in obj.completed_steps.all()])
-
-    get_completed_steps.short_description = 'Пройденные этапы'
-
-
+@admin.register(OnboardingText)
 class OnboardingTextAdmin(admin.ModelAdmin):
-    list_display = ['part_number', 'title', 'content']
-
-
-class ChatMessageAdmin(admin.ModelAdmin):
-    list_display = ['user', 'message']
-    list_filter = ['created_at']
-
-
-class ComplaintAdmin(admin.ModelAdmin):
-    list_display = ['user', 'text']
-
-
-admin.site.register(OnboardingText, OnboardingTextAdmin)
-admin.site.register(ChatMessage, ChatMessageAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
+    list_display = ('content', 'order')
+    list_display_links = ('content',)
+    list_editable = ('order',)
--- onboarding/urls.py
+++ onboarding/urls.py
@@ -1,20 +1,8 @@
 from django.urls import path
-
-from . import views
-from .views import OnboardingTextAPIView, OnboardingTextDetailView, \
-    ChatMessageView, ComplaintView
+from .views import OnboardingTextView
 
 urlpatterns = [
-    path('onboarding-text/', OnboardingTextAPIView.as_view(),
-         name='onboarding-text-list-create'),
-    path('onboarding-text/<int:pk>/', OnboardingTextDetailView.as_view(),
-         name='onboarding-text-detail'),
-    path('chat-message/', ChatMessageView.as_view(),
-         name='chat-message-list-create'),
-    path('complaint/', ComplaintView.as_view(), name='complaint-list-create'),
-    path('onboarding/steps/', views.OnboardingStepAPIView.as_view(),
-         name='onboarding-steps'),
-    path('user/onboarding/<int:user_id>/',
-         views.UserOnboardingAPIView.as_view(), name='user-onboarding'),
+    path('text/', OnboardingTextView.as_view(), name='onboarding-text')
 
+    # Добавьте другие URL-маршруты по мере необходимости
 ]
